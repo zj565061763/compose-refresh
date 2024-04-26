afd9893f0676977de3f43c544133eb55409ceae5
zjun <565061763@qq.com>
2024-04-26 22:05:46 +0800

    cancelNotifyCallbackJob

--- lib/src/main/java/com/sd/lib/compose/refresh/FRefreshState.kt
+++ lib/src/main/java/com/sd/lib/compose/refresh/FRefreshState.kt
@@ -174,6 +174,7 @@ internal class RefreshStateImpl(
 
     override fun showRefresh() {
         coroutineScope.launch(_dispatcher) {
+            cancelNotifyCallbackJob()
             animateToRefreshing()
         }
     }
