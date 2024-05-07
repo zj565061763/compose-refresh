f53241b2d123b847ca932ffa07beeb777998c956
zjun <565061763@qq.com>
2024-05-07 23:53:49 +0800

    1.0.0-beta01

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-refresh"
-val libVersionName = "1.0.0-alpha05"
+val libVersionName = "1.0.0-beta01"
 
 android {
     namespace = "com.sd.lib.compose.refresh"
